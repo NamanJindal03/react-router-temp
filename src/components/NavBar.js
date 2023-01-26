aa43f7471cfaa53c93a412007c706a9368221984
SONU SINGH <sonusingh82342@gmail.com>
2023-01-27 00:16:59 +0530

    new_commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,20 @@ import React from 'react';
 
 export const NavBar = () =>{
     return(
+        <nav>
+        <ul>
+          <li>
+            <NavLink className="index-link" activeClassName="active" exact to="/">
+              Index
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="home-link" activeClassName="active" exact to="/home">
+              Home
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    );
 
-    )
 }
