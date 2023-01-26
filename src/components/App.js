d7fcf5338b3965a18d9e323fd60719dca10f2c95
SONU SINGH <sonusingh82342@gmail.com>
2023-01-27 00:22:37 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from '../AppRoutes';
 import '../styles/App.css';
 import { NavBar } from './NavBar';
+import { Home } from '../Pages/Home';
+import { Index } from '../Pages/Index';
+import { NotFound } from '../Pages/NotFound';
 const App = () => {
 
   return (
