519905076de34d0a176d533f8d6de56860838695
SONU SINGH <sonusingh82342@gmail.com>
2023-02-01 00:15:08 +0530

    new4

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Route , Switch } from 'react-router-dom';
+import { Route, Switch  } from 'react-router-dom';
 import { Home } from './Pages/Home';
 import { Index } from './Pages/Index';
 import { NotFound } from './Pages/NotFound';
 
+
 export const AppRoutes = () => {
     return (
-    <Switch>
-<Route exact path="/" component={Index} />
-<Route exact path="/home" component={Home} />  
-<Route  component={NotFound} /> 
-</Switch>
+        <Switch>
+            <Route exact path="/" element = { <Index/>} />
+            <Route exact path="/home" element = { <Home/>} /> 
+            <Route element={<NotFound/>}/> 
+
+        </Switch>
+    
 
     )
 }
